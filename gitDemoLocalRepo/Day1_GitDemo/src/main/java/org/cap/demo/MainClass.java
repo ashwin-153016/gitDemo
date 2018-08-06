b926f573cee16f5114bd524970abd52b9057df0c
ashwir <ashwir@DIN73002990.corp.capgemini.com>
2018-08-06 10:36:50 +0530

    My first commit

---
+++ gitDemoLocalRepo/Day1_GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,14 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+		greet();
+	}
+
+	public static void greet() {
+		System.out.println("Hello ");
+	}
+}
